984463768b5e79e640eec70cb383d9bca222c4e0
manseo00 <skaalstj0615@naver.com>
2026-06-23 17:05:41 +0900

    update footer logo and institute text

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -4,11 +4,11 @@ export default function SiteFooter() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerInner}>
-        {/* public/images/jeju-logo.png -> /images/jeju-logo.png */}
+        {/* public/images/jeju-footer-logo.png -> /images/jeju-footer-logo.png */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           className={styles.footerLogo}
-          src="/images/jeju-logo.png"
+          src="/images/jeju-footer-logo.png"
           alt="제주특별자치도"
         />
         <span className={styles.footerName}>해양수산연구원</span>
